f1ff208f4340e4dd74b076226459d60ff0042e66
Andrew Jackson <andrew@disvelop.net>
2019-12-21 14:02:33 +1100

    changed color scheme and dmenu

--- config.def.h
+++ config.def.h
@@ -1,5 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
+#include <X11/XF86keysym.h>
+
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 5;        /* gaps between windows */
@@ -12,17 +14,40 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
+
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_bg[]               = "#1b1918";
+static const char col_fg[]               = "#f1efee";
+static const char col_grey[]             = "#a8a19f";
+static const char col_blue[]             = "#407ee7";
+
+static const char color00[] = "#303030";  // black
+static const char color01[] = "#c23b2d";  // red
+static const char color02[] = "#2aaf4d";  // green
+static const char color03[] = "#e7940f";  // yellow
+static const char color04[] = "#207ab5";  // blue
+static const char color05[] = "#9442b6";  // magenta
+static const char color06[] = "#12a689";  // cyan
+static const char color07[] = "#a9a9a9";  // white
+static const char color08[] = "#4d4d4d";  // black b
+static const char color09[] = "#ec4c3c";  // red b
+static const char color10[] = "#30d052";  // green b
+static const char color11[] = "#f3c714";  // yellow b
+static const char color12[] = "#2f97de";  // blue b
+static const char color13[] = "#a75ec5";  // magenta b
+static const char color14[] = "#0ecca7";  // cyan b
+static const char color15[] = "#d3d3d3";  // white b
+
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_grey,  col_bg,    col_bg },
+	[SchemeSel]  = { color13,  col_bg,    color13  },
 };
 static const unsigned int alphas[][3]      = {
 	/*               fg      bg        border     */
@@ -51,9 +76,9 @@ static int attachbelow = 1;    /* 1 means attach after the currently active wind
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+	{ "[t]",      tile },    /* first entry is default */
+	{ "[f]",      NULL },    /* no layout function means floating behavior */
+	{ "[m]",      monocle },
 };
 
 /* key definitions */
@@ -69,8 +94,12 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", color13, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "terminator", NULL };
+static const char *mutecmd[]       = { "amixer", "-q", "set", "Master", "toggle", NULL };
+static const char *volupcmd[]      = { "amixer", "-q", "set", "Master", "2%+", NULL };
+static const char *voldncmd[]      = { "amixer", "-q", "set", "Master", "2%-", NULL };
+
 
 #include "movestack.c"
 static Key keys[] = {
@@ -104,6 +133,9 @@ static Key keys[] = {
 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
+	{ 0,                       XF86XK_AudioLowerVolume, spawn, {.v = voldncmd } },
+	{ 0,                       XF86XK_AudioMute, spawn, {.v = mutecmd } },
+	{ 0,                       XF86XK_AudioRaiseVolume, spawn, {.v = volupcmd   } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
